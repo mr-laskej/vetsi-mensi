9fbc2261b9010d46f21e5f55dfcf0d069b471ede
MatyL <MatyL@DESKTOP-J9HAJU7>
2021-10-24 11:28:49 +0200

    Přidejte soubory projektu.

---
+++ JEBNE MI/JEBNE MI.cpp
@@ -0,0 +1,19 @@
+﻿
+
+#include <stdio.h>
+
+	int main(void)
+	{
+		int x = 2;
+		int y = 4;
+		printf("Cislo %d ", x);
+		if (x > y) {
+			printf("je vetsi ");
+		}
+		else {
+			printf("je mensi ");
+		}
+		printf("nez %d. \n", y);
+	}
+
+
